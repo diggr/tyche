84c1ea673ebecaa67632014fb3dc70ab624df882
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2019-06-17 16:43:23 +0200

    add tyche_test for 100% coverage

---
+++ tyche/tests/test_tyche.py
@@ -0,0 +1,11 @@
+from ..tyche import ProvitChecker
+
+TEST_FILENAME = "invalid_testfile"
+
+def test_provit_checker_on_invalid_prov_file(tmp_path):
+    tmp_path.joinpath(TEST_FILENAME).touch()
+    with open(tmp_path.joinpath(f"{TEST_FILENAME}.prov"), "w") as prov_file:
+        prov_file.write("{}")
+    assert ProvitChecker.check_path(tmp_path) == False
+
+
